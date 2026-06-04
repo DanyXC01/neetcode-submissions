1dfe9c83eb57c569c523b73bfcfcdb73bf76a496
danyx <danilbelov0303@mail.ru>
2026-06-05 00:34:58 +0300

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations: string[]): number {
+        const stack = []
+
+        const sum = (numbers: number[]): number => {
+            return numbers.reduce((acc, el) => acc + el, 0)
+        }
+
+        for (let op of operations) {
+            switch (op) {
+                case "D":
+                    const last = stack[stack.length - 1]
+                    const modifiedLast = last * 2
+                    stack.push(modifiedLast)
+                    break;
+                case "+":
+                    const lastTwoScores = stack.slice(-2, stack.length)
+                    const stackSum = sum(lastTwoScores)
+                    stack.push(stackSum)
+                    break;
+                case "C":
+                    stack.pop()
+                    break;
+                default:
+                    stack.push(Number(op))
+            }
+        }
+
+
+        return sum(stack)
+    }
+}
