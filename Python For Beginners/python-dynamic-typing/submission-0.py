cc94a428be3dcc66674ce658e1944bc79d1cadc0
danyx <danilbelov0303@mail.ru>
2026-06-20 15:53:49 +0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 2.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "str"
+print(type(variable))
+variable = []
+print(type(variable))
