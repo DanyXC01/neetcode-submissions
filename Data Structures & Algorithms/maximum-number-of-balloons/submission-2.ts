b213d0240564e1ea7c6d114825d79ab5b3dc73e0
danyx <danilbelov0303@mail.ru>
2026-06-25 14:45:57 +0300

    Add: maximum-number-of-balloons - submission-2

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-2.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} text
+     * @return {number}
+     */
+    maxNumberOfBalloons(text: string): number {
+        const target = "balloon"; // 1 1 2 2 1
+        const map: Map<string, number> = new Map();
+
+        for (let char of text) {
+            if (target.includes(char) && map.has(char)) {
+                map.set(char, (map.get(char) || 0) + 1);
+            } else if (target.includes(char)) {
+                map.set(char, 1);
+            }
+        }
+
+        const b = map.get('b') || 0;
+        const a = map.get('a') || 0;
+        const l = Math.floor((map.get('l') || 0) / 2);
+        const o = Math.floor((map.get('o') || 0) / 2);
+        const n = map.get('n') || 0;
+
+        return Math.min(b, a, l, o, n);
+    }
+}
